79123a75ccddf9795817663a99ca9a379ce2a4cd
Jamie Morgan <jamiemorgan2013@gmail.com>
2023-02-26 19:52:55 +0000

    updated home page to show how many bulbs are connected, added more to base.html, added styles

--- home/views.py
+++ home/views.py
@@ -1,5 +1,21 @@
+import asyncio
+import os
 from django.shortcuts import render
+from tplinkcloud import TPLinkDeviceManager
+from dotenv import load_dotenv
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'home.html')
+    load_dotenv()
+
+    device_manager = TPLinkDeviceManager(
+        os.getenv("email"), os.getenv("password"))
+
+    devices = {}
+
+    all_devices = asyncio.run(device_manager.get_devices())
+
+    for device in all_devices:
+        devices[device.get_alias()] = device
+
+    return render(request, 'home.html', {'devices': devices})
